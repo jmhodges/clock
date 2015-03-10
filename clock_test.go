0bde282e01a622110af5ecd40ebde036a196282f
Jeff Hodges <jeff@somethingsimilar.com>
2015-03-09 17:32:51 -0700

    add small examples for Clock and FakeClock

--- clock_test.go
+++ clock_test.go
@@ -1,6 +1,7 @@
 package clock
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -15,4 +16,33 @@ func TestFakeClockGoldenPath(t *testing.T) {
 	if clk.Now().Equal(second.Now()) {
 		t.Errorf("clocks different must differ: %#v vs %#v", clk.Now(), second.Now())
 	}
+}
+
+func ExampleClock() {
+	c := Default()
+	now := c.Now()
+	fmt.Println(now.UTC().Zone())
+	// Output:
+	// UTC 0
+}
+
+func ExampleFakeClock() {
+	c := Default()
+	fc := NewFake()
+	fc.Add(20 * time.Hour)
+	fc.Add(-5 * time.Minute) // negatives work, as well
+
+	if fc.Now().Equal(fc.Now()) {
+		fmt.Println("FakeClocks' Times always equal themselves.")
+	}
+	if !c.Now().Equal(fc.Now()) {
+		fmt.Println("Clock and FakeClock can be set to different times.")
+	}
+	if !fc.Now().Equal(NewFake().Now()) {
+		fmt.Println("FakeClocks work independently, too.")
+	}
+	// Output:
+	// FakeClocks' Times always equal themselves.
+	// Clock and FakeClock can be set to different times.
+	// FakeClocks work independently, too.
 }
